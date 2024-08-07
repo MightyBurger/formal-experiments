c6cbfa7db833692fb05d04a13f31e096113376e7
Jordan Johnson <52584279+MightyBurger@users.noreply.github.com>
2024-08-06 20:43:14 -0600

    make a little more clear

--- hdl/counter.vhd
+++ hdl/counter.vhd
@@ -4,8 +4,7 @@ use ieee.numeric_std.all;
 
 entity counter is
 generic (
-    GWIDTH: natural;
-    CMAX: natural
+    GWIDTH: natural
 );
 port (
     clk: in std_logic;
@@ -23,7 +22,12 @@ end counter;
 
 architecture rtl of counter is
 
+    constant COUNT_MIN: natural := 0;
+    constant COUNT_MAX: natural := 2**GWIDTH - 1;
+
 begin
+
+    -- counter logic
 
     process(clk, arst_n)
     begin
@@ -33,23 +37,25 @@ begin
             if rst_n = '0' then
                 count <= (others => '0');
             else
-                if incr = '1' and decr = '0' and count < CMAX then
+                if incr = '1' and decr = '0' and full = '0' then
                     count <= count + 1;
-                elsif decr = '1' and incr = '0' and count > 0 then
+                elsif decr = '1' and incr = '0' and empty = '0' then
                     count <= count - 1;
                 end if;
             end if;
         end if;
     end process;
 
+    -- status flags
+
     process(all)
     begin
-        if count < 1 then
+        if count <= COUNT_MIN then
             empty <= '1';
         else
             empty <= '0';
         end if;
-        if count >= CMAX then
+        if count >= COUNT_MAX then
             full <= '1';
         else
             full <= '0';
